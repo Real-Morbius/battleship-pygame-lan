438b387990c71855b0b0f0e4d55ab461f6113ed2
Szymon P <szymon_jozef@proton.me>
2026-04-03 20:04:07 +0200

    feat: update init with new modules

--- src/battleship_pygame_lan/logic/__init__.py
+++ src/battleship_pygame_lan/logic/__init__.py
@@ -1,5 +1,5 @@
-from .boards import Board, Radar
 from .enums import FieldState, ShipType, ShotResult
 from .models import Ship
+from .player import Player
 
-__all__ = ["Board", "ShipType", "FieldState", "Ship", "ShotResult", "Radar"]
+__all__ = ["ShipType", "FieldState", "Ship", "ShotResult", "Player"]
